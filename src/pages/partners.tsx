052d4e76bee0b75de6b52d9f3651d5390f7e97f4
leoreisdias <leonardord99@gmail.com>
2021-07-12 23:06:25 -0300

    feat: SSR to get partners list

--- src/pages/partners.tsx
+++ src/pages/partners.tsx
@@ -1,35 +1,33 @@
-import { useEffect, useState, useCallback } from 'react';
-
+import { GetServerSideProps, GetServerSidePropsContext } from 'next';
 import Link from 'next/link';
+import { parseCookies } from 'nookies';
 
 import { api } from '../services/api';
 import { Container, ListItem, ListItemButtonContainer } from '../styles/pages/Partners';
 import { formOptions } from '../types/formOptions';
 import { PartnersProps } from '../types/interfaces/Partners';
 
-export default function Partners() {
-  const [partners, setPartners] = useState<PartnersProps[]>();
+interface PartnersServer {
+  partnersList: PartnersProps[];
+}
 
-  const loadPartners = useCallback(async () => {
+export default function Partners({ partnersList }: PartnersServer) {
+  async function handleDisablePartner(id: string) {
     try {
-      const { data } = await api.get('/partners');
-      console.log(data);
-
-      setPartners(data);
+      await api.patch(`/partners/${id}`, {
+        active: false,
+      });
     } catch (err) {
-      console.log(err);
+      //...
     }
-  }, []);
-
-  useEffect(() => {
-    loadPartners();
-  }, [loadPartners]);
+  }
 
   return (
     <Container>
+      <h3>Parceiros Cadastrados</h3>
       <ul>
-        {partners &&
-          partners.map((partner) => {
+        {partnersList &&
+          partnersList.map((partner) => {
             return (
               <Link
                 href={`/writer-area?update=${formOptions.partners}&id=${partner._id}`}
@@ -42,7 +40,11 @@ export default function Partners() {
                     <strong>{partner.paymentDay}</strong>
                   </span>
                   <ListItemButtonContainer>
-                    <button type="button" className={partner.active ? 'active' : ''}>
+                    <button
+                      type="button"
+                      className={partner.active ? 'active' : ''}
+                      onClick={() => handleDisablePartner(partner._id)}
+                    >
                       {partner.active ? 'Desativar' : 'Ativar'}
                     </button>
                     <button type="button">Editar</button>
@@ -55,3 +57,33 @@ export default function Partners() {
     </Container>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async (
+  ctx: GetServerSidePropsContext
+) => {
+  const { token } = parseCookies(ctx);
+
+  try {
+    const { data } = await api.get('/fullpartners', {
+      headers: {
+        authorization: 'Bearer ' + token,
+      },
+    });
+    const partnersList = data.sort((item) => item.active);
+    console.log(partnersList);
+
+    return {
+      props: {
+        partnersList,
+      },
+    };
+  } catch (err) {
+    console.log(err);
+    // return {
+    //   redirect: {
+    //     destination: '/',
+    //     permanent: false,
+    //   },
+    // };
+  }
+};
--- src/styles/pages/Partners.ts
+++ src/styles/pages/Partners.ts
@@ -2,6 +2,9 @@ import styled from 'styled-components';
 
 export const Container = styled.section`
   display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 2rem;
 
   max-width: 90%;
 
