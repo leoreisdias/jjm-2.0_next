495adce2fa0d1e3e654af85014be9393bf55e4f0
leoreisdias <leonardord99@gmail.com>
2021-08-16 23:11:25 -0300

    feat: Keeping this serverless function here for future solutions

---
+++ src/components/WhiteBackdrop/WhiteBackdropStyle.ts
@@ -0,0 +1,19 @@
+import { motion } from 'framer-motion';
+import { rgba } from 'polished';
+import styled from 'styled-components';
+
+export const WhiteBackdropWrapper = styled(motion.div)`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100%;
+  z-index: 1 !important;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  background: ${(props) => rgba(props.theme.colors.transitionBackground, 0.99)};
+  color: #fff;
+`;
---
+++ src/components/WhiteBackdrop/index.tsx
@@ -0,0 +1,23 @@
+import { AnimatePresence } from 'framer-motion';
+import BeatLoader from 'react-spinners/BeatLoader';
+
+import { SlowFadeInOut } from '../../assets/motion/Variants';
+import { useTheme } from '../../hooks/useTheme';
+import { WhiteBackdropWrapper } from './WhiteBackdropStyle';
+
+export const WhiteBackdrop = () => {
+  const { colors } = useTheme();
+
+  return (
+    <AnimatePresence exitBeforeEnter>
+      <WhiteBackdropWrapper
+        variants={SlowFadeInOut}
+        initial="begin"
+        animate="animate"
+        exit="exit"
+      >
+        <BeatLoader color={colors.jjmPallete_1} size={30} />
+      </WhiteBackdropWrapper>
+    </AnimatePresence>
+  );
+};
