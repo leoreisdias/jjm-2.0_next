9be3b9933b8a73eb019c253d2c920a487575fdac
leoreisdias <leonardord99@gmail.com>
2022-08-07 21:41:48 -0300

    fix: static news description

--- src/pages/complete-news/[hash].tsx
+++ src/pages/complete-news/[hash].tsx
@@ -383,7 +383,7 @@ export const getStaticProps: GetStaticProps = async ({
       subjects: news.subjects,
       id: news._id,
       title: news.title,
-      description: news.description,
+      description: news.description ?? '',
       date: format(parseISO(news.createdAt), 'dd/MM/yyyy', {
         locale: ptBR,
       }),
